0876a23e9b0d4a681adf39a5ff554c37b59ba661
alumno <alumno@Equipo1-PC>
2017-05-12 10:48:36 +0200

    Modificaciones

--- src/Vista/VisorImagen.java
+++ src/Vista/VisorImagen.java
@@ -51,7 +51,6 @@ public class VisorImagen extends javax.swing.JDialog {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Así ser verá la imagen en el catálogo");
-        setUndecorated(true);
         setResizable(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
